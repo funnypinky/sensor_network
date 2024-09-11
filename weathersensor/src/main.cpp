789bae1c03ab56ed7f79a8f022c172e210bd18ec
Funnypinky <steffenhasler@googlemail.com>
2024-09-11 09:19:13 +0200

    Change to lorawan

--- weathersensor/src/main.cpp
+++ weathersensor/src/main.cpp
@@ -3,14 +3,20 @@
 #include <MS5611.h>
 #include "esp_adc_cal.h"
 #include "WiFi.h"
-#include <ESP32Time.h>
 #include <esp_task_wdt.h>
 #include <vector>
-#include "mesh.hpp"
+#include "config_lorawan.h"
 #include <esp32-hal-adc.h>
 
 #include <cmath>
 #include "math.h"
+#include <RadioLib.h>
+#include <Preferences.h>
+Preferences store;
+
+RTC_DATA_ATTR uint16_t bootCount = 0;
+RTC_DATA_ATTR uint16_t bootCountSinceUnsuccessfulJoin = 0;
+RTC_DATA_ATTR uint8_t LWsession[RADIOLIB_LORAWAN_SESSION_BUF_SIZE];
 
 // I2C PINS
 #define SDA1 21
@@ -23,10 +29,6 @@
 MS5611 ms5611(0x77);
 
 #define SHT31_ADDRESS 0x44
-
-#define uS_TO_S_FACTOR 1000000 /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP 110      /* Time ESP32 will go to sleep (in seconds) */
-#define WDT_TIMEOUT 150
 
 struct
 {
@@ -48,19 +50,17 @@ const int potPin = 36;
 int analogBattery = A0;
 int g_vref = 1100;
 
-ESP32Time rtc;
-RTC_DATA_ATTR long lastSync = 0;
-
-Mesh mesh;
+int16_t state = 0;
 
 /*Prototypes*/
+int16_t lwActivate();
+void gotoSleep(uint32_t seconds);
 void printHeaterStatus(uint16_t status);
 void readSht();
 void get_battery();
 unsigned long getTime();
 void messureTask();
 float round(float value, int precision);
-void syncTime();
 std::vector<String> splitString(String string, char delim);
 
 void setup()
@@ -70,66 +70,44 @@ void setup()
   btStop();
   Serial.begin(9600);
   Wire.begin(SDA1, SCL1);
+  Serial.println(F("Initalise the radio"));
+  state = radio.begin();
+  debug(state != RADIOLIB_ERR_NONE, F("Initalise radio failed"), state, true);
 
-  esp_task_wdt_init(WDT_TIMEOUT, true); // enable panic so ESP32 restarts
-  esp_task_wdt_add(NULL);
+  // activate node by restoring session or otherwise joining the network
+  state = lwActivate();
 
-  Serial.println();
-  Serial.println("Temperature sensor measuring start");
-
-  if (mesh.initMesh())
-  {
-  }
-  Serial.println(lastSync);
-  if ((lastSync == 0) || ((rtc.getLocalEpoch() - lastSync) > 43200))
-  {
-    syncTime();
-    lastSync = rtc.getLocalEpoch();
-    Serial.println("time sync");
-  }
-  Serial.println("init ok");
-
-  esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   messureTask();
-  delay(2000);
-  mesh.sleep();
-  
-  esp_task_wdt_reset();
-  esp_deep_sleep_start();
+
+  uint8_t *persist = node.getBufferSession();
+  memcpy(LWsession, persist, RADIOLIB_LORAWAN_SESSION_BUF_SIZE);
+  gotoSleep(uplinkIntervalSeconds);
 }
 
 void messureTask()
 {
+  uint8_t payload[8];
   get_battery();
-  String txStr;
-  txStr += "data;";
-  txStr += WiFi.macAddress() + ";";
-  txStr += String(voltages.battery) + ";";
-  txStr += String(voltages.panel) + ";";
+  node.setDeviceStatus(voltages.battery);
   if (sht.begin())
   {
     readSht();
-    txStr += String(temperature) + ";";
-    txStr += String(humidity) + ";";
-  }
-  else
-  {
-    txStr += ";";
-    txStr += ";";
+    int tmp = ((int)(temperature * 100)) + 27315;
+    payload[0] = tmp >> 8;
+    payload[1] = tmp;
+    int hum = (int)(humidity * 2);
+    payload[2] = hum;
   }
   if (ms5611.begin())
   {
     ms5611.setOversampling(OSR_ULTRA_HIGH);
     ms5611.read();
-    txStr += String(ms5611.getPressure()) + ";";
+    int pre = (int)(ms5611.getPressure() * 10);
+    payload[3] = pre >> 8;
+    payload[4] = pre;
   }
-  else
-  {
-    txStr += ";";
-  }
-  txStr += String(rtc.getLocalEpoch()) + ";";
-  Serial.println(txStr);
-  mesh.sendMessage(txStr);
+
+  node.uplink(payload,4, true);
 }
 void loop() {}
 
@@ -146,19 +124,6 @@ void readSht()
   sht.read(false);
   humidity = sht.getHumidity();
 }
-void printHeaterStatus(uint16_t status)
-{
-  Serial.print(millis());
-  Serial.print("\tHEATER: ");
-  if (status & SHT31_STATUS_HEATER_ON)
-  {
-    Serial.println("ON");
-  }
-  else
-  {
-    Serial.println("OFF");
-  }
-}
 
 uint32_t readADC_cal(int ADC_Raw)
 {
@@ -174,12 +139,6 @@ void get_battery()
   voltages.battery = (readADC_cal(analogRead(ADC_BAT)) / 0.192) / 1000;
   voltages.panel = (readADC_cal(analogRead(ADC_PANEL)) / 0.05475) / 1000;
 }
-unsigned long getTime()
-{
-  time_t now;
-  time(&now);
-  return now;
-}
 
 float round(float value, int precision)
 {
@@ -187,39 +146,110 @@ float round(float value, int precision)
   return (float)((int)(value * multiplier + 0.5f)) / multiplier;
 }
 
-void syncTime()
+
+void gotoSleep(uint32_t seconds)
 {
-  bool timeSync = false;
-  String reqTime;
-  reqTime += "reqTime;";
-  reqTime += WiFi.macAddress() + ";";
-  mesh.sendMessage(reqTime);
-  while (!timeSync)
-  {
-    String answer = mesh.onReceive(LoRa.parsePacket());
-    if (answer != "")
-    {
-      std::vector<String> measVec = splitString(answer, ';');
-      if (measVec[0] == "repTime")
-      {
-        rtc.setTime(atol(measVec[2].c_str()));
-        Serial.println(rtc.getTime());
-      }
-      timeSync = true;
-    }
-  }
+  esp_sleep_enable_timer_wakeup(seconds * 1000UL * 1000UL); // function uses uS
+  Serial.println(F("Sleeping\n"));
+  Serial.flush();
+
+  esp_deep_sleep_start();
+
+  // if this appears in the serial debug, we didn't go to sleep!
+  // so take defensive action so we don't continually uplink
+  Serial.println(F("\n\n### Sleep failed, delay of 5 minutes & then restart ###\n"));
+  delay(5UL * 60UL * 1000UL);
+  ESP.restart();
 }
 
-std::vector<String> splitString(String string, char delim)
+int16_t lwActivate()
 {
-  String temp = string.substring(0, string.length());
-  std::vector<String> returnVector;
-  int index = string.indexOf(delim);
-  while (index != -1)
+  int16_t state = RADIOLIB_ERR_UNKNOWN;
+
+  // setup the OTAA session information
+  node.beginOTAA(joinEUI, devEUI, nwkKey, appKey);
+
+  Serial.println(F("Recalling LoRaWAN nonces & session"));
+  // ##### setup the flash storage
+  store.begin("radiolib");
+  // ##### if we have previously saved nonces, restore them and try to restore session as well
+  if (store.isKey("nonces"))
   {
-    returnVector.push_back(temp.substring(0, index));
-    temp = temp.substring(index + 1, string.length());
-    index = temp.indexOf(delim);
+    uint8_t buffer[RADIOLIB_LORAWAN_NONCES_BUF_SIZE];                   // create somewhere to store nonces
+    store.getBytes("nonces", buffer, RADIOLIB_LORAWAN_NONCES_BUF_SIZE); // get them from the store
+    state = node.setBufferNonces(buffer);                               // send them to LoRaWAN
+    debug(state != RADIOLIB_ERR_NONE, F("Restoring nonces buffer failed"), state, false);
+
+    // recall session from RTC deep-sleep preserved variable
+    state = node.setBufferSession(LWsession); // send them to LoRaWAN stack
+
+    // if we have booted more than once we should have a session to restore, so report any failure
+    // otherwise no point saying there's been a failure when it was bound to fail with an empty LWsession var.
+    debug((state != RADIOLIB_ERR_NONE) && (bootCount > 1), F("Restoring session buffer failed"), state, false);
+
+    // if Nonces and Session restored successfully, activation is just a formality
+    // moreover, Nonces didn't change so no need to re-save them
+    if (state == RADIOLIB_ERR_NONE)
+    {
+      Serial.println(F("Succesfully restored session - now activating"));
+      state = node.activateOTAA();
+      debug((state != RADIOLIB_LORAWAN_SESSION_RESTORED), F("Failed to activate restored session"), state, true);
+
+      // ##### close the store before returning
+      store.end();
+      return (state);
+    }
   }
-  return returnVector;
+  else
+  { // store has no key "nonces"
+    Serial.println(F("No Nonces saved - starting fresh."));
+  }
+
+  // if we got here, there was no session to restore, so start trying to join
+  state = RADIOLIB_ERR_NETWORK_NOT_JOINED;
+  while (state != RADIOLIB_LORAWAN_NEW_SESSION)
+  {
+    Serial.println(F("Join ('login') to the LoRaWAN Network"));
+    state = node.activateOTAA();
+
+    // ##### save the join counters (nonces) to permanent store
+    Serial.println(F("Saving nonces to flash"));
+    uint8_t buffer[RADIOLIB_LORAWAN_NONCES_BUF_SIZE];                   // create somewhere to store nonces
+    uint8_t *persist = node.getBufferNonces();                          // get pointer to nonces
+    memcpy(buffer, persist, RADIOLIB_LORAWAN_NONCES_BUF_SIZE);          // copy in to buffer
+    store.putBytes("nonces", buffer, RADIOLIB_LORAWAN_NONCES_BUF_SIZE); // send them to the store
+
+    // we'll save the session after an uplink
+
+    if (state != RADIOLIB_LORAWAN_NEW_SESSION)
+    {
+      Serial.print(F("Join failed: "));
+      Serial.println(state);
+
+      // how long to wait before join attempts. This is an interim solution pending
+      // implementation of TS001 LoRaWAN Specification section #7 - this doc applies to v1.0.4 & v1.1
+      // it sleeps for longer & longer durations to give time for any gateway issues to resolve
+      // or whatever is interfering with the device <-> gateway airwaves.
+      uint32_t sleepForSeconds = min((bootCountSinceUnsuccessfulJoin++ + 1UL) * 60UL, 3UL * 60UL);
+      Serial.print(F("Boots since unsuccessful join: "));
+      Serial.println(bootCountSinceUnsuccessfulJoin);
+      Serial.print(F("Retrying join in "));
+      Serial.print(sleepForSeconds);
+      Serial.println(F(" seconds"));
+
+      gotoSleep(sleepForSeconds);
+
+    } // if activateOTAA state
+  } // while join
+
+  Serial.println(F("Joined"));
+
+  // reset the failed join count
+  bootCountSinceUnsuccessfulJoin = 0;
+
+  delay(1000); // hold off off hitting the airwaves again too soon - an issue in the US
+
+  // ##### close the store
+  store.end();
+  return (state);
 }
